158b996fb0c49d305798aed28abcd90099e00ff7
Caiyi Hsu <cai1hsu@outlook.com>
2021-11-13 21:41:19 +0800

    rewrite for c#

--- Program.cs
+++ Program.cs
@@ -15,25 +15,22 @@ string[] units = new string[]{"个","十","百","千","万"};
 
 using (StreamWriter w = new StreamWriter("./main.cpp",true)){
     
-    w.WriteLine("#include <iostream>");
-    w.WriteLine("using namespace std;");
-    w.WriteLine("int main(){");
-    w.WriteLine("\tint x = 0;");
-    w.WriteLine("\tcout << \"请给出一个不多于5位的整数:\";");
-    w.WriteLine("\tcin >> x;");
-    w.WriteLine("\tswitch(x){");
+    w.WriteLine("using System;");
+    w.WriteLine("int x = 0;");
+    w.WriteLine("Console.Write(\"请给出一个不多于5位的整数:\");");
+    w.WriteLine("x = Convert.ToInt32(Console.ReadLine())");
+    w.WriteLine("switch(x){");
 
     for(var i = 0;i < 100000;i++){
-        w.WriteLine($"\t\tcase {i}:");
+        w.WriteLine($"\tcase {i}:");
         string stri = i.ToString();
         int len = stri.Length;
-        w.WriteLine($"\t\t\tcout << \"是{len}位数\" << endl;");
+        w.WriteLine($"\t\tConsole.WriteLine(\"是{len}位数\");");
         for(var d = 0;d < len;d++){
-            w.WriteLine($"\t\t\tcout << \"{units[d]}位是: {stri[d]}\" << endl;");
+            w.WriteLine($"\tConsole.WriteLine(\"{units[d]}位是: {stri[d]}\");");
         }
-        w.WriteLine($"\t\t\tcout << \"倒过来是: {Reverse(i)}\" << endl;");
-        w.WriteLine("\t\t\tbreak;");
+        w.WriteLine($"\t\tConsole.WriteLine(\"倒过来是: {Reverse(i)}\");");
+        w.WriteLine("\t\tbreak;");
     }
-
-    w.Write("\t}\n\treturn 0;\n}");
+    w.Write("}");
 }
